6c524cce3302e5f5ec75fef55b78f625b33022b2
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-11-29 16:31:16 +0100

    Update app

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const fs = require("node:fs");
+
 app.get("/", (req, res) => {
   res.send("Hello, Kubernetes!");
+  const name = process.env.USER_NAME || "name unset";
+  const content = `Hello, ${name}! \n`;
+  fs.writeFile("./out.log", content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("File written successfully");
+    }
+  });
 });
 
 app.listen(port, () => {
